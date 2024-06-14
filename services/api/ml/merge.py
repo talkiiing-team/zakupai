19c34afb44b2199c038d7065111c2ae19dfb4db1
Nerlih Max <nerlihmax@yandex-team.ru>
2024-06-15 05:36:59 +0700

    fix

--- services/api/ml/merge.py
+++ services/api/ml/merge.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 import pandas as pd
 
 from lib.merge_contracts import ContractsMerger
@@ -20,12 +21,10 @@ pays_df = pd.concat(map(lambda x: pd.read_excel(x), pays_paths), axis=0)
 print('loaded all dataframes')
 
 merger = ContractsMerger(pays_df, merger_df, main_costs_df, squares_df, serv_codes_df)
+res, features = merger.start_merging()
 
-a, b = merger.start_merging()
+print('merged')
 
-print(a, b)
-
-
-# res.to_csv("res_datetimes.csv")
-# with open("features.pkl", "wb") as f:
-#     pickle.dump(features, f)
+res.to_csv("/mnt/bucket/res_datetimes.csv")
+with open("/mnt/bucket/features.pkl", "wb") as f:
+    pickle.dump(features, f)
