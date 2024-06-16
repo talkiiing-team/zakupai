d2b0f039188eb17d26232d93c8842d30a3e9cf56
Nerlih Max <nerlihmax@yandex-team.ru>
2024-06-16 18:05:24 +0700

    memoize inventory

--- services/web/src/features/graph-designer/ui/inventory.tsx
+++ services/web/src/features/graph-designer/ui/inventory.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import clsx from 'clsx';
 
 import { NodeColor, mapColor } from '@/features/graph-designer/model/color';
@@ -13,33 +13,36 @@ type Props = {
 };
 
 export const Inventory: FC<Props> = ({ inventory }) => {
+    const items = useMemo(
+        () => [...inventory].sort((a, b) => (a.color < b.color ? 1 : -1)),
+        [inventory],
+    );
+
     return (
         <div className="flex flex-col gap-4 overflow-scroll border-r border-zinc-300 bg-white p-4">
-            {inventory
-                .sort((a, b) => (a.color < b.color ? 1 : -1))
-                .map(({ type, displayName, defaultData, color }) => (
-                    <div
-                        className={clsx(
-                            'rounded border border-zinc-300 p-2 text-center shadow-md',
-                            mapColor[color],
-                        )}
-                        onDragStart={(event) => {
-                            event.dataTransfer.setData(
-                                'application/reactflow-type',
-                                type,
-                            );
-                            event.dataTransfer.setData(
-                                'application/reactflow-data',
-                                JSON.stringify(defaultData),
-                            );
-                            event.dataTransfer.effectAllowed = 'move';
-                        }}
-                        key={type}
-                        draggable
-                    >
-                        {displayName}
-                    </div>
-                ))}
+            {items.map(({ type, displayName, defaultData, color }) => (
+                <div
+                    className={clsx(
+                        'rounded border border-zinc-300 p-2 text-center shadow-md',
+                        mapColor[color],
+                    )}
+                    onDragStart={(event) => {
+                        event.dataTransfer.setData(
+                            'application/reactflow-type',
+                            type,
+                        );
+                        event.dataTransfer.setData(
+                            'application/reactflow-data',
+                            JSON.stringify(defaultData),
+                        );
+                        event.dataTransfer.effectAllowed = 'move';
+                    }}
+                    key={type}
+                    draggable
+                >
+                    {displayName}
+                </div>
+            ))}
         </div>
     );
 };
