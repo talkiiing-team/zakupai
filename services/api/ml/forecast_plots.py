e25ab6824a526cffbea6f3db0e2087342773a1c2
Nerlih Max <nerlihmax@yandex-team.ru>
2024-06-17 00:23:02 +0700

    wip

--- services/api/ml/forecast_plots.py
+++ services/api/ml/forecast_plots.py
@@ -6,12 +6,12 @@ from lib.graphs import generate_interactive_plots_all_data, generate_interactive
 
 check_id = int(sys.argv[1])
 
-forecast_df = pd.read_csv('/mnt/bucket/distribution_result.csv')
+distribution_df = pd.read_csv('/mnt/bucket/distribution_result.csv')
 
-generate_interactive_plots_all_data(forecast_df)
-generate_interactive_plots(forecast_df, check_id = check_id)
+generate_interactive_plots_all_data(distribution_df)
+generate_interactive_plots(distribution_df, check_id=check_id)
 
-plots = generate_interactive_plots_notspecf_finaltable(forecast_df)
+plots = generate_interactive_plots_notspecf_finaltable(distribution_df)
 
 with open('/mnt/bucket/forecast_plots.json', 'w') as f:
     json.dump(plots, f, ensure_ascii=False)
