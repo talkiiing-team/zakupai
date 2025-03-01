2c250d14acba91e0d98c9a73fedbf14e010238dc
Georgiy Panin <novastream2030@ya.ru>
2025-03-01 23:55:42 +0800

    fix: mobile

--- web/src/components/navigation/aside-header.tsx
+++ web/src/components/navigation/aside-header.tsx
@@ -1,13 +1,14 @@
 import type { PropsWithChildren } from 'react';
 
 import { useAtom } from 'jotai';
-import { AsideHeader as AsideHeaderBase, FooterItem } from '@gravity-ui/navigation';
+import { AsideHeader as AsideHeaderBase, MobileHeader } from '@gravity-ui/navigation';
 import { useLocation, useNavigate } from '@tanstack/react-router';
 import { ChartAreaStackedNormalized, ClockArrowRotateLeft, PaperPlane } from '@gravity-ui/icons';
 
 import BrandLogo from '~/assets/logo.svg?react'
 
 import { navigationCompactAtom } from '~/atoms/navigation';
+import { IS_MOBILE } from '@/shared/config';
 
 export interface AsideHeaderProps {}
 
@@ -16,6 +17,61 @@ export function AsideHeader({ children }: PropsWithChildren<AsideHeaderProps>) {
 
   const location = useLocation()
   const navigate = useNavigate()
+
+  if (IS_MOBILE)
+    return (
+      <MobileHeader
+        logo={{
+          text: 'ЗакупAI',
+          icon: BrandLogo,
+          href: '#',
+          onClick: () => navigate({ to: '/' }),
+        }}
+  
+        renderContent={() => children}
+  
+        burgerMenu={{
+          items: [
+            {
+              id: 'dashboards',
+              title: 'Дашборды',
+              icon: ChartAreaStackedNormalized,
+              onItemClick: () => navigate({ to: '/dashboards' }),
+              current: location.pathname.includes('/dashboards')
+            },
+            {
+              id: 'notification-channels',
+              title: 'Каналы нотификаций',
+              icon: PaperPlane,
+              onItemClick: () => navigate({ to: '/notification-channels' }),
+              current: location.pathname.includes('notification-channels')
+            },
+            {
+              id: 'schedulers',
+              title: 'Планировщики',
+              icon: ClockArrowRotateLeft,
+              onItemClick: () => navigate({ to: '/schedulers' }),
+              current: location.pathname.includes('schedulers')
+            },
+          ]
+        }}
+  
+        // renderFooter={
+        //   ({ compact }) => (
+        //     <>
+        //       <FooterItem
+        //         compact={compact}
+        //         item={{
+        //           id: 'Да',
+        //           title: 'Да',
+        //           tooltipText: 'Minor issue (Now)',
+        //         }}
+        //       />
+        //     </>
+        //   )
+        // }
+      />
+    );
 
   return (
     <AsideHeaderBase
@@ -40,7 +96,7 @@ export function AsideHeader({ children }: PropsWithChildren<AsideHeaderProps>) {
             title: 'Дашборды',
             icon: ChartAreaStackedNormalized,
             onItemClick: () => navigate({ to: '/dashboards' }),
-            current: location.pathname.startsWith('/dashboards')
+            current: location.pathname.includes('/dashboards')
           }
         },
         {
