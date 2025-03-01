8db1bb9de16311d657635ef701edce9e87ef06a5
Nerlih Max <nerlihmax@yandex-team.ru>
2025-03-01 20:26:07 +0300

    cors wildcard

--- api/app/main.py
+++ api/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:5173", "https://закуп-ай.рф"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
